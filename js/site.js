86fdea0ecefc66e50cbd9bf85a11e43088e41736
Michael Loerop <maloerop@gmail.com>
2022-01-19 22:14:42 -0500

    Updated pages. Working on script

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function helloWorld () {
-    alert("Hello World");
+
+//Get starting and ending values
+function getValues () {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    let numbers = generateNumbers(startValue, endValue);
 }
+
+//Generate numbers from startValue to endValue
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+
+    for (let i = startValue; i <= endValue; i++) {
+        numbers.push(index);
+    }
+
+    return numbers;
+}
+
+//Display the numbers and mark the even numbers bold
