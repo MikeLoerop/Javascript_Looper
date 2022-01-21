ef009f04ee9212596290ff998505e3e89297d458
Michael Loerop <maloerop@gmail.com>
2022-01-20 22:26:21 -0500

    Finished the js code. Polishing site.

--- js/site.js
+++ js/site.js
@@ -3,11 +3,21 @@
 function getValues () {
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
+    
 
+    //Parse inputs into integers
     startValue = parseInt(startValue);
     endValue = parseInt(endValue);
 
-    let numbers = generateNumbers(startValue, endValue);
+    
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    }else{
+        alert("You must enter integers");
+    }
+
+    
 }
 
 //Generate numbers from startValue to endValue
@@ -15,10 +25,31 @@ function generateNumbers(startValue, endValue){
     let numbers = [];
 
     for (let i = startValue; i <= endValue; i++) {
-        numbers.push(index);
+        numbers.push(i);
     }
 
     return numbers;
 }
 
 //Display the numbers and mark the even numbers bold
+function displayNumbers(numbers) {
+    let templateRow = "";
+    
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+
+        let className = "even";
+        if(number % 2 == 0){
+            className
+        }else{
+
+            className = "odd";
+            
+        }
+
+        templateRow += `<tr><td class="${className}">${number}</td><tr>`;
+                
+    }
+
+    document.getElementById("results").innerHTML = templateRow;
+}
